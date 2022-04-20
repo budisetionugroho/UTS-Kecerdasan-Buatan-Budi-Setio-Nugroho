131afa776be040a6b20fe0a5216ddd66b13fad4d
Budi Setio Nugroho <budi.setio@mail.sirclo.com>
2022-04-20 13:35:23 +0700

    Pengulangan UTS

---
+++ Main.py
@@ -0,0 +1,21 @@
+#while
+print("Pengulangan menggunakan while")
+i = 1
+while i < 10:
+    print(i)
+    i+=1
+print()
+#for
+print("Pengulangan for menggunakan list")
+angka = [5,4,3,2,1]
+for a in angka:
+    print(f"angka sekarang adalah {a}")
+
+print("")
+
+print("Pengulangan for menggunakan range")
+angka2 = range(5)
+for b in angka2:
+    print(f"angka sekarang adalah {b}")
+
+print("akhir dari program")
